7be9bbc50ec569ca05c16a5d9c7a7cd1ccd3b08c
Rafael <rafaelsciacco@gmail.com>
2018-08-14 16:56:34 -0300

    validacoes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
 import { Component } from '@angular/core';
 
 @Component({
@@ -6,5 +7,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'case1';
+  rForm: FormGroup;
+  post: any;
+  phone = '';
+  name = '';
+
+  constructor(private fb: FormBuilder) {
+
+    this.rForm = fb.group({
+      'name': [null, Validators.required],
+      'phone': ['Ex:(11)4002-8922', Validators.compose([Validators.required, Validators.minLength(10)])],
+      'email': ['Ex: nome@email.com'],
+      'idade': [null],
+      'cidade': [null],
+      'estado': [null],
+      'usuario': [null],
+      'senha': [null],
+      'validate': ''
+    });
+
+  }
+
+  addPost(post) {
+    this.name = post.name;
+    this.phone = post.phone;
+  }
 }
